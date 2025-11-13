55c88def7af35c6bfe7386f324125bc263ffaa55
yusufsw <yusuf-talha@outlook.com>
2025-11-13 23:48:18 +0300

    Added pagination

--- StoreApp/StoreApp.Web/Controllers/HomeController.cs
+++ StoreApp/StoreApp.Web/Controllers/HomeController.cs
@@ -6,24 +6,34 @@ namespace StoreApp.Web.Controllers;
 
 public class HomeController : Controller
 {
+    public int pageSize = 3;
     private IStoreRepsository _storeRepsository;
     public HomeController(IStoreRepsository storeRepository)
     {
         _storeRepsository = storeRepository;
     }
-    public IActionResult Index()
+    public IActionResult Index(int page = 1)
     {
-        var products = _storeRepsository.Products.Select(p => new ProductViewModel
+        var products = _storeRepsository
+            .Products
+            .Skip((page -1) * pageSize)//oteleme 
+            .Select(p =>
+            new ProductViewModel
         {
             Id = p.Id,
             Name = p.Name,
             Description = p.Description,
             Price = p.Price
-        }).ToList();
+        }).Take(pageSize);
 
         return View(new ProductListViewModel
         {
-            Products = products
+            Products = products,
+            PageInfo = new PageInfo
+            {
+                ItemsPerPage = pageSize,
+                TotalItems = _storeRepsository.Products.Count(),
+            }
         });
     }
     
