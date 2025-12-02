605c208288d34840e5d29cbe1f5f63ba4c680d84
yusufsw <yusuf-talha@outlook.com>
2025-12-02 03:21:35 +0300

    Added CartSummaryViewComponent

---
+++ StoreApp/StoreApp.Web/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using StoreApp.Web.Models;
+
+namespace StoreApp.Web.Components;
+
+public class CartSummaryViewComponent: ViewComponent
+{
+    private Cart cart;
+    public CartSummaryViewComponent(Cart cartService)
+    {
+        cart = cartService;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        return View(cart);
+    }
+}
